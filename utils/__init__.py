ffb15412f389adb300b6943c71d7b3d98874d53d
黄超 <thss15_huangc@163.com>
2020-02-06 17:34:04 +0800

    Adjust print format

--- utils/__init__.py
+++ utils/__init__.py
@@ -117,13 +117,13 @@ def create_xiaohuashan_workbook(data):
     worksheet.center_vertically()
     worksheet.set_paper(9)
     worksheet.set_row(0, 70)
-    worksheet.set_column('A:A', 15)
-    worksheet.set_column('B:B', 15)
+    worksheet.set_column('A:A', 10)
+    worksheet.set_column('B:B', 10)
     worksheet.set_column('C:C', 100)
     title_format = workbook.add_format({
         "font_name": "宋体",
         "bold": True,
-        "font_size": 24,
+        "font_size": 20,
         "align": "center",
         "valign": "vcenter",
     })
@@ -131,28 +131,28 @@ def create_xiaohuashan_workbook(data):
     worksheet.merge_range(0, 0, 0, 2, data['title'], title_format)
     header_format = workbook.add_format({
         "font_name": "宋体",
-        "font_size": 20,
+        "font_size": 16,
         "border": 1,
         "align": "center",
         "valign": "vcenter",
     })
     cell_format = workbook.add_format({
         "font_name": "宋体",
-        "font_size": 20,
+        "font_size": 16,
         "border": 1,
         "align": "left",
         "valign": "vcenter",
     })
     cell_format.set_text_wrap()
-    worksheet.set_row(1, 50)
-    worksheet.set_row(2, 50)
-    worksheet.set_row(3, 50)
-    worksheet.set_row(4, 50)
-    worksheet.set_row(5, 50)
-    worksheet.set_row(6, 50)
-    worksheet.set_row(7, 50)
-    worksheet.set_row(8, 50)
-    worksheet.set_row(9, 50)
+    worksheet.set_row(1, 40)
+    worksheet.set_row(2, 40)
+    worksheet.set_row(3, 40)
+    worksheet.set_row(4, 40)
+    worksheet.set_row(5, 40)
+    worksheet.set_row(6, 40)
+    worksheet.set_row(7, 40)
+    worksheet.set_row(8, 40)
+    worksheet.set_row(9, 40)
     worksheet.merge_range(1, 0, 1, 1, '整治项目', header_format)
     worksheet.merge_range(2, 0, 2, 1, '时间', header_format)
     worksheet.merge_range(3, 0, 3, 1, '地点', header_format)
@@ -161,7 +161,7 @@ def create_xiaohuashan_workbook(data):
     worksheet.merge_range(6, 0, 7, 0, '经费', header_format)
     worksheet.write(6, 1, '材料机械', header_format)
     worksheet.write(7, 1, '人工', header_format)
-    worksheet.merge_range(8, 0, 8, 1, '误餐情况', header_format)
+    worksheet.merge_range(8, 0, 8, 1, '其他', header_format)
     worksheet.merge_range(9, 0, 9, 1, '合计', header_format)
     content = json.loads(data['content'])
     date_data = data['accounting_date'].split('-')
